125a613a338e2c50620028d171d89df210862968
QuocAnh189 <anhquoc18092003@gmail.com>
2025-02-18 17:02:08 +0700

    update

--- src/__init__.py
+++ src/__init__.py
@@ -18,26 +18,35 @@ def create_app():
             return {"error": "No image provided"}, 400
 
         # Call the model's predict function
-        plate_list, crop_img_list = model.predict(license_plate)  
+        plate_list, crop_img_list = model.predict(license_plate) 
+        print(len(plate_list))
 
-        cv2.imwrite("crop.jpg", crop_img_list[0])
-        crop_response=cloudinary.uploader.upload(
-        'crop.jpg',
-        folder="parking",
-        unique_filename = True, 
-        overwrite=True,
-        eager=[{"width": 200, "crop": "fill"}])
-        os.remove('crop.jpg')
+        if len(plate_list) != 1:
+            if len(plate_list) == 0:
+                return jsonify({
+                    "message": "There are not one plate",
+                })
+            else:
+                return jsonify({
+                    "message": "There are more one plate",
+                })
+        else:
+            crop_url = ''
+            if (len(crop_img_list) == 1):
+                cv2.imwrite("crop.jpg", crop_img_list[0])
+                crop_response=cloudinary.uploader.upload(
+                'crop.jpg',
+                folder="parking",
+                unique_filename = True, 
+                overwrite=True,
+                eager=[{"width": 200, "crop": "fill"}])
+                os.remove('crop.jpg')
+                crop_url = crop_response["url"]
 
-        return jsonify({
-            "license_plate_detect": list(plate_list),
-            "crop_img_list": crop_response["url"],
-        })
+            return jsonify({
+                "license_plate_detect": list(plate_list),
+                "crop_img_list": crop_url,
+            })
 
-    
-        # return jsonify({
-        #     "license_plate_detect": "license_plate_detect",
-        #     "crop_img_list": "crop_img_list"
-        # })
-
+        
     return app
